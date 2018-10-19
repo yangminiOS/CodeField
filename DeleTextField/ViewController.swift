8cba3b1229c2f0198ef8231b2048822c2864115a
yangmin <yangmin@silvrr.com>
2018-10-19 11:00:44 +0800

    解决卡顿现象

--- DeleTextField/ViewController.swift
+++ DeleTextField/ViewController.swift
@@ -17,8 +17,6 @@ class ViewController: UIViewController, TaxInputViewDelegate {
         
         taxInputView = TaxInputView()
         taxInputView.delegate = self
-        //taxInputView.activeColor = UIColor.init(red: 245.0/255, green: 106.0/255, blue: 32.0/255, alpha: 1.0)
-        
         view.addSubview(taxInputView)
      
         taxInputView.frame = CGRect.init(x: 20, y: 100, width: view.frame.width - 40, height: 35)
